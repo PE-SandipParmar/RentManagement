98881b0908614a9558865c729710b9328396b58c
Sandip Parmar <sandip.koradia@pe.solutions>
2025-09-01 10:31:23 +0530

    folder code uncommented

--- Program.cs
+++ Program.cs
@@ -104,12 +104,12 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-//app.UseStaticFiles(new StaticFileOptions
-//{
-//    FileProvider = new PhysicalFileProvider(
-//        Path.Combine(builder.Environment.ContentRootPath, "Content")),
-//    RequestPath = "/Content"
-//});
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "Content")),
+    RequestPath = "/Content"
+});
 app.UseRouting();
 
 // Security headers
